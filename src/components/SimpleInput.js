80b6976bdbc123ca1435d56053382d6eaefa56ca
Inokcode <regilearn@gmail.com>
2021-11-03 19:07:19 +0530

    useRef and useState method added

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,12 +2,32 @@ import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
-  // const ref = useRef(initialValue);
+  const nameInputRef = useRef();
+  //
+  const nameInputChangHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  //
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log({ enteredValue });
+    setEnteredName(''); //you cant do this in useRef 2
+    // nameInputChangHandler.current.value ="" this is not work bcz we dont direclt manipulate dom
+  };
+  //
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
